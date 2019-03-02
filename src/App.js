a47f7565de0bf2360fa5b5a79a2ec640ee083cab
John Obi <johngorithm@gmail.com>
2019-03-02 13:07:36 +0100

    chore(setup): setup static files for the project
    - add bootstrap css
    - add font awesome
    - add header and detail fonts
    - include static images
    - include data source file
    - add default styles to App.css
    
    [Delivers #001]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">Card One</div>
+          <div className="col-6">
+            <span>
+              <i className="fas fa-home"></i>
+            </span>
+          </div>
+        </div>
       </div>
     );
   }
